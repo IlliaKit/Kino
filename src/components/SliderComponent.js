a0091679ef87ef1b96382ff35140f70a4afd1cde
IlliaKit <kitilla7@gmail.com>
2023-05-03 13:05:07 +0200

    add films list

--- src/components/SliderComponent.js
+++ src/components/SliderComponent.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+
 import { filmsList } from "../helpers/filmsList";
 
 export default class MultipleItems extends Component {
   render() {
     const settings = {
       className: "center",
-      dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 4,
-      slidesToScroll: 3,
+      slidesToScroll: 2,
     };
     return (
       <div>
         <Slider {...settings}>
-          {filmsList.map((filmsList, index) => (
-            <div>
-              <h3>{filmsList.title}</h3>
+          {filmsList.map((filmsList) => (
+            <div class="container">
               <img src={filmsList.url} alt="" />
+              <div>
+                <p className="slideText">{filmsList.title}</p>
+                <p className="seansText">Session Schedule</p>
+
+                <p className="seansTime">
+                  {filmsList.time
+                    ? ` ${filmsList.time}`
+                    : `Sorry, sessions no more`}
+                </p>
+              </div>
             </div>
           ))}
         </Slider>
--- src/helpers/filmsList.js
+++ src/helpers/filmsList.js
@@ -1,50 +1,54 @@
+import Guardians_of_the_Galaxy_Vol_3_poster from "./../img/Guardians_of_the_Galaxy_Vol_3_poster.jpg";
+import the_little_mermaid from "./../img/the_little_mermaid.jpg";
 const filmsList = [
   {
     id: 1,
-    url: "https://via.placeholder.com/300x200/FF0000/FFFFFF",
-    title: "Image 1",
+    url: Guardians_of_the_Galaxy_Vol_3_poster,
     description: "Description for Image 1",
+    title: "Guardians of the Galaxy",
+    time: [" Mn-19:00", " We-20:00"],
   },
   {
     id: 2,
-    url: "https://via.placeholder.com/300x200/00FF00/FFFFFF",
-    title: "Image 2",
+    url: the_little_mermaid,
+    title: "The little mermaid",
     description: "Description for Image 2",
+    time: "Premiere May 25",
   },
   {
     id: 3,
-    url: "https://via.placeholder.com/300x200/0000FF/FFFFFF",
+    url: Guardians_of_the_Galaxy_Vol_3_poster,
     title: "Image 3",
     description: "Description for Image 3",
   },
   {
     id: 3,
-    url: "https://via.placeholder.com/300x200/0000FF/FFFFFF",
-    title: "Image 3",
+    url: Guardians_of_the_Galaxy_Vol_3_poster,
+    title: "Image 4",
     description: "Description for Image 3",
   },
   {
     id: 3,
     url: "https://via.placeholder.com/300x200/0000FF/FFFFFF",
-    title: "Image 3",
+    title: "Image 5",
     description: "Description for Image 3",
   },
   {
     id: 3,
     url: "https://via.placeholder.com/300x200/0000FF/FFFFFF",
-    title: "Image 3",
+    title: "Image 6",
     description: "Description for Image 3",
   },
   {
     id: 3,
     url: "https://via.placeholder.com/300x200/0000FF/FFFFFF",
-    title: "Image 3",
+    title: "Image 7",
     description: "Description for Image 3",
   },
   {
     id: 3,
     url: "https://via.placeholder.com/300x200/0000FF/FFFFFF",
-    title: "Image 3",
+    title: "Image 8",
     description: "Description for Image 3",
   },
 ];
